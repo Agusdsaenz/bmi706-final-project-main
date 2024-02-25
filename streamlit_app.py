d5ff0f70ee5097aebb59a5464a2fb6c34ae1686f
Agusdsaenz <112197184+Agusdsaenz@users.noreply.github.com>
2024-02-25 12:28:28 -0500

    box plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,18 @@ df = pd.read_csv('Medicare_Hospital_Spending_Per_Patient-Hospital.csv')
 selected_states = ['MA', 'NY']
 selected_hospitals = ['BOSTON MEDICAL CENTER', 'MASSACHUSETTS GENERAL HOSPITAL', 'CAMBRIDGE HEALTH ALLIANCE']
 
-colors = ['red', 'green', 'blue']  
+colors = ['red', 'green', 'blue']
 hospital_to_color = {hospital: color for hospital, color in zip(selected_hospitals, colors)}
 
+
+def apply_color(row):
+    return hospital_to_color.get(row['Facility Name'], 'black')
+
+df['Color'] = df.apply(apply_color, axis=1)
+
+
 df_filtered = df[df['State'].isin(selected_states)]
+
 
 base = alt.Chart(df_filtered).encode(
     y=alt.Y('Score:Q', axis=alt.Axis(labels=False)),
@@ -24,7 +32,6 @@ box_plot = base.mark_boxplot().encode(
     color=alt.value('lightgray'),
     opacity=alt.value(0.5)
 )
-
 
 dots = base.mark_circle(size=10).encode(  
     color=alt.Color('Color:N', legend=None),
