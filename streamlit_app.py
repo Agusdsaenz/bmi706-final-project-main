02dcb959bc32f18812d41dd13594e635de7f5e17
Agusdsaenz <112197184+Agusdsaenz@users.noreply.github.com>
2024-02-25 13:46:10 -0500

    box plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,30 +22,32 @@ df_filtered = df[df['State'].isin(selected_states)].copy()
 df_filtered['Score'] = pd.to_numeric(df_filtered['Score'], errors='coerce')
 
 
+df_sorted = df_filtered.groupby('State').apply(lambda x: x.sort_values(by='Score', ascending=False)).reset_index(drop=True)
 
 base = alt.Chart(df_filtered).encode(
-    y='Score:Q',
-    x='Facility Name:N',
-    color='Color:N',
+    y=alt.Y('Score:Q', axis=alt.Axis(title='Medicare Spending per Beneficiary', labels=False)),
+    x=alt.X('Facility Name:N', axis=alt.Axis(title='Hospitals', labels=False)),  
     tooltip=['Facility Name', 'Score']
 ).properties(
     width=450
 )
 
-dots = base.mark_circle(size=60).encode(
-    opacity=alt.value(1)
+dots = base.mark_circle(size=60).encode(  
+    color=alt.Color('Color:N', legend=None),
+    opacity=alt.value(1),
 )
 
+
 final_chart = dots.facet(
-    column='State:N',
-    header=alt.Header(
-        labelOrient='bottom',
-        titleOrient='bottom'
-    ),
-    spacing=5
-).transform_window(
+    column=alt.Column('State:N', header=alt.Header(labelOrient='bottom', titleOrient='bottom')),
+    spacing=5,
+)
+
+
+final_chart = final_chart.transform_window(
     rank='rank(Score)',
     sort=[alt.SortField('Score', order='descending')]
 )
+
 
 final_chart
