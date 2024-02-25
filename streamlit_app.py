26cb3fd1b155586347584f22d987bb0ba7edc12b
Agusdsaenz <112197184+Agusdsaenz@users.noreply.github.com>
2024-02-25 14:18:10 -0500

    box plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,16 @@ def apply_color(row):
 
 df['Color'] = df.apply(apply_color, axis=1)
 
+df_filtered = df[df['State'].isin(selected_states)].copy()
 
-df_filtered = df[df['State'].isin(selected_states)].copy() 
+
 df_filtered['Score'] = pd.to_numeric(df_filtered['Score'], errors='coerce')
 
 
 df_sorted = df_filtered.groupby('State').apply(lambda x: x.sort_values(by='Score', ascending=False)).reset_index(drop=True)
 
-base = alt.Chart(df_filtered).encode(
+
+base = alt.Chart(df_sorted).encode(
     y=alt.Y('Score:Q', axis=alt.Axis(title='Medicare Spending per Beneficiary', labels=False), scale=alt.Scale(domain=[0.5, 1.8])),
     x=alt.X('Facility Name:N', axis=alt.Axis(title='Hospitals', labels=False)),  
     tooltip=['Facility Name', 'Score']
@@ -46,12 +48,4 @@ final_chart = dots.facet(
     spacing=5,
 )
 
-
-final_chart = final_chart.transform_window(
-    rank='rank(Score)',
-    sort=[alt.SortField('Score', order='descending')]
-)
-
-
 final_chart
-
