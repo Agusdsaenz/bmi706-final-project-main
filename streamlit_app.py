d6830a51bcad2210e686b7d5c214bcbb0cc1cb0c
ninabgold <ngold@mgh.harvard.edu>
2024-02-28 16:30:41 -0500

    Changing the relative size of the map and bar chart again

--- streamlit_app.py
+++ streamlit_app.py
@@ -260,7 +260,7 @@ fig_bar.update_layout(height=desired_height)
 
 st.header("Medicare Spending Score per State")
 
-col1, col2 = st.columns(2) 
+col1, col2 = st.columns([3,1]) 
 
 with col1:
     st.plotly_chart(fig, use_container_width=True)
