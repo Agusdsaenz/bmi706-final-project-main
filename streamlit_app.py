23fd972db63e74865c48d190c5494aafd5b33c86
Agusdsaenz <112197184+Agusdsaenz@users.noreply.github.com>
2024-02-28 22:42:24 -0500

    plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,14 @@ median_line = alt.Chart(df_sorted).mark_rule(color='red', size=2).encode(
     y='median(Score):Q'
 )
 
-final_chart = alt.layer(dots, median_line).facet(
+median_label = base.mark_text(
+    align='left', dx=5, dy=-5, color='red', size=12
+).encode(
+    text=alt.Text('median(Score):Q', format='.2f'),  
+    y='median(Score):Q'
+)
+
+final_chart = alt.layer(dots, median_line, median_label ).facet(
     column=alt.Column('State:N', header=alt.Header(labelOrient='bottom', titleOrient='bottom')),
     spacing=40
 ).configure_axis(
