ad45caa62bca5b8f1add61eeb3ac7e21a0ced008
Agusdsaenz <112197184+Agusdsaenz@users.noreply.github.com>
2024-03-04 21:59:07 -0500

    plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -436,7 +436,7 @@ st.header("US Medicare Spending per Beneficiary by State")
 col1, col2 = st.columns([6,2]) 
 
 with col1:
-    st.plotly_chart(fig, use_container_width=True, config={"staticPlot": True})
+    st.plotly_chart(fig, use_container_width=True, config={"staticPlot": False})
 
 with col2:
     st.plotly_chart(fig_bar, use_container_width=True)
